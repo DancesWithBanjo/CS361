3999d9aedbc2b52265938e807f8fe97372b48bba
Christian Wiemer <cawiemer922@gmail.com>
2017-10-20 19:19:07 -0700

    hashtable class finished (I hope)

--- ASMT4/Table.java
+++ ASMT4/Table.java
@@ -9,15 +9,17 @@ public class Table<K, V>
 {
     public ArrayList<ArrayList<Node<K,V>>> table;
     
-    private static int[] primes = {11, 19, 41, 79, 163, 317, 641, 1279, 2557, 5119, 10243, 20479, 40961, 81919, 163841, 327673};
+    private static  final int[] primes = {11, 19, 41, 79, 163, 317, 641, 1279, 2557, 5119, 10243, 20479, 40961, 81919, 163841, 327673};
     private int size = 0; //track when putting in NEW item
     private int pIndex = 0;
     private int prime = primes[pIndex];
-    private double lFactor = size/pIndex;
-    private double maxLoad = 0.75;
+    private double lFactor;
+    private final double MAXLOAD = 0.75;
     
     /**
      * Constructor for objects of class Table
+     * 
+     * @params length: a prime number used for the array's size
      */
     public Table(int length)
     {
@@ -26,11 +28,19 @@ public class Table<K, V>
         }
     }
     
+    /**
+     * Hashes the key and places the key-value pair in a position based on the hashcode.
+     * If the key is already there, it will replace the value stored in there with the new one.
+     * Triggers rehash() if the load factor is greater than 0.75.
+     * 
+     * @params key: the key linked with the value
+     * @params value: the value assigned to the key
+     */
     public void put(K key, V value){
         int hash = key.hashCode() % prime;
         ArrayList<Node<K,V>> hashArray = table.get(hash);
-        
         boolean replaced = false;
+        
         for(int i = 0; i<hashArray.size(); i++){
             if(hashArray.get(i).getKey().equals(key)){
                 hashArray.get(i).value = value;
@@ -38,16 +48,23 @@ public class Table<K, V>
             }
         }
         
-        if(replaced){
+        if(!replaced){
             hashArray.add(new Node(key,value));
             size++;
         }
         
-        if(lFactor > maxLoad){
-            
+        lFactor = size/prime;
+        
+        if(lFactor > MAXLOAD){
+            rehash();
         }
     }
     
+    /**
+     * Finds and returns the value of the input key.
+     * 
+     * @params key: the key who's value is being sought after
+     */
     public V get(K key){
         int hash = key.hashCode() % prime;
         ArrayList<Node<K,V>> hashArray = table.get(hash);
@@ -63,6 +80,11 @@ public class Table<K, V>
         return val;
     }
     
+    /**
+     * Checks to see if a key is in the hash table.
+     * 
+     * @params key: the key to check for
+     */
     public boolean contains(K key){
         int hash = key.hashCode() % prime;
         ArrayList<Node<K,V>> hashArray = table.get(hash);
@@ -78,6 +100,11 @@ public class Table<K, V>
         return truth;
     }
     
+    /**
+     * Removes the input key from the hash table.
+     * 
+     * @params key: the key to delete
+     */
     public void delete(K key){
         int hash = key.hashCode() % prime;
         ArrayList<Node<K,V>> hashArray = table.get(hash);
@@ -89,25 +116,31 @@ public class Table<K, V>
         }
     }
     
+    /**
+     * Returns the total number of key-value pairs in the hash table
+     */
     public int size(){
         return size;
     }
     
+    //Rehashes the hash table with a higher prime number
     private void rehash(){
         pIndex++;
         prime = primes[pIndex];
         
         ArrayList<ArrayList<Node<K,V>>> tempTable = table;
+        table = new ArrayList<ArrayList<Node<K,V>>>();
         for(int i = 0; i<prime; i++){
             table.add(i, new ArrayList<Node<K,V>>());
         }
+        
+        size = 0;
         
         for(int i = 0; i<tempTable.size(); i++){
             for(int j = 0; j<tempTable.get(i).size(); j++){
                 if(tempTable.get(i).get(j) == null){
                 } else{
-                    table.get(i).add(j, new Node(tempTable.get(i).get(j).getKey(), tempTable.get(i).get(j).getValue()));
-                    //This is definitely not right
+                    put(tempTable.get(i).get(j).getKey(), tempTable.get(i).get(j).getValue());
                 }
             }
         }
